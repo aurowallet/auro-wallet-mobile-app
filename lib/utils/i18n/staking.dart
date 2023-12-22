83e3477d13274c17549f80a14dd44eea1b357947
wjdfx <wjdfxa@gmail.com>
2023-12-23 00:47:06 +0800

    remove unuse file

--- lib/utils/i18n/staking.dart
+++
@@ -1,9 +0,0 @@
-const Map<String, String> zhStaking = {
-  "delegators": "{count} 委托者",
-  "submitList": "提交/更新节点到这个列表",
-};
-
-const Map<String, String> enStaking = {
-  "delegators": "{count} Delegators",
-  "submitList": "Submit/Update your node to this list",
-};
