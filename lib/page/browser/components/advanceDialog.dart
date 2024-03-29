2da9ac7c186d036f39265d2f40f1a2c2b895f1a2
lvshaoping007 <1244849816@qq.com>
2024-03-29 19:39:39 +0800

    fix broser inject page ui

--- lib/page/browser/components/advanceDialog.dart
+++ lib/page/browser/components/advanceDialog.dart
@@ -64,76 +64,80 @@ class _AdvanceDialogState extends State<AdvanceDialog> {
   @override
   Widget build(BuildContext context) {
     AppLocalizations dic = AppLocalizations.of(context)!;
-    return Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(12),
-              topLeft: Radius.circular(12),
-            )),
-        padding: EdgeInsets.only(left: 0, top: 8, right: 0, bottom: 16),
-        child: SafeArea(
-          child: Stack(
-            children: [
-              Wrap(
+
+    return Padding(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(12),
+                  topLeft: Radius.circular(12),
+                )),
+            padding: EdgeInsets.only(left: 0, top: 8, right: 0, bottom: 16),
+            child: SafeArea(
+              child: Stack(
                 children: [
-                  BrowserDialogTitleRow(
-                      title: dic.advanceMode, showCloseIcon: true),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: InputItem(
-                      label: dic.feePlaceHolder,
-                      maxLength: 16,
-                      initialValue: '',
-                      placeholder: widget.nextStateFee is double
-                          ? widget.nextStateFee.toString()
-                          : "",
-                      padding: EdgeInsets.only(top: 20),
-                      controller: _feeCtrl,
-                      inputFormatters: [
-                        UI.decimalInputFormatter(COIN.decimals)
-                      ],
-                      keyboardType:
-                          TextInputType.numberWithOptions(decimal: true),
-                      labelStyle: TextStyle(
-                        fontWeight: FontWeight.w700,
+                  Wrap(
+                    children: [
+                      BrowserDialogTitleRow(
+                          title: dic.advanceMode, showCloseIcon: true),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: InputItem(
+                          label: dic.feePlaceHolder,
+                          maxLength: 16,
+                          initialValue: '',
+                          placeholder: widget.nextStateFee is double
+                              ? widget.nextStateFee.toString()
+                              : "",
+                          padding: EdgeInsets.only(top: 20),
+                          controller: _feeCtrl,
+                          inputFormatters: [
+                            UI.decimalInputFormatter(COIN.decimals)
+                          ],
+                          keyboardType:
+                              TextInputType.numberWithOptions(decimal: true),
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  InputErrorTip(
-                    padding: EdgeInsets.only(top: 8, left: 20, right: 20),
-                    ctrl: _feeCtrl,
-                    message: dic.feeTooLarge,
-                    keepShow: false,
-                    validate: _validateFee,
-                    tipType: TipType.warn,
-                    hideIcon: true,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: InputItem(
-                      label: "Nonce",
-                      initialValue: '',
-                      placeholder: widget.nonce.toString(),
-                      padding: EdgeInsets.only(top: 20),
-                      controller: _nonceCtrl,
-                      inputFormatters: <TextInputFormatter>[
-                        FilteringTextInputFormatter.digitsOnly
-                      ],
-                      keyboardType: TextInputType.number,
-                      labelStyle: TextStyle(
-                        fontWeight: FontWeight.w700,
+                      InputErrorTip(
+                        padding: EdgeInsets.only(top: 8, left: 20, right: 20),
+                        ctrl: _feeCtrl,
+                        message: dic.feeTooLarge,
+                        keepShow: false,
+                        validate: _validateFee,
+                        tipType: TipType.warn,
+                        hideIcon: true,
                       ),
-                    ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: InputItem(
+                          label: "Nonce",
+                          initialValue: '',
+                          placeholder: widget.nonce.toString(),
+                          padding: EdgeInsets.only(top: 20),
+                          controller: _nonceCtrl,
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          keyboardType: TextInputType.number,
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      ZkAppBottomButton(
+                        onConfirm: onConfirm,
+                        hideCancel: true,
+                      ),
+                    ],
                   ),
-                  ZkAppBottomButton(
-                    onConfirm: onConfirm,
-                    hideCancel: true,
-                  )
                 ],
               ),
-            ],
-          ),
-        ));
+            )));
   }
 }
