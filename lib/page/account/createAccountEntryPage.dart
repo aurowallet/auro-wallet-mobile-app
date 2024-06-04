276245f6eb0ab24b3fa81a0881bd6072adb8dfb0
wjdfx <wjdfxa@gmail.com>
2024-06-04 11:22:44 +0800

    Update CreateAccountEntryPage ui

--- lib/page/account/createAccountEntryPage.dart
+++ lib/page/account/createAccountEntryPage.dart
@@ -12,23 +12,25 @@ class CreateAccountEntryPage extends StatelessWidget {
   static final String route = '/account/entry';
   final SettingsStore store;
   final Function changeLang;
-  BuildContext? _ctx;
-  void _onClick(String type) async {
+  void _onClick(BuildContext _ctx, String type) async {
     bool? agree = await showDialog<bool?>(
-      context: _ctx!,
+      context: _ctx,
       builder: (_) {
-        return TermsDialog(store: store,);
+        return TermsDialog(
+          store: store,
+        );
       },
     );
     if (agree == true) {
-      Navigator.pushNamed(_ctx!, SetNewWalletPasswordPage.route, arguments:{"type": type});
+      Navigator.pushNamed(_ctx, SetNewWalletPasswordPage.route,
+          arguments: {"type": type});
     }
   }
+
   @override
   Widget build(BuildContext context) {
     AppLocalizations dic = AppLocalizations.of(context)!;
     var theme = Theme.of(context).textTheme;
-    _ctx = context;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: null,
@@ -42,43 +44,20 @@ class CreateAccountEntryPage extends StatelessWidget {
               Expanded(
                 child: Column(
                   children: [
-                    SizedBox(height: 50,),
+                    SizedBox(
+                      height: 50,
+                    ),
                     Row(
                       children: [
-                        Expanded(child: Stack(
-                          children: [
-                            Positioned(
-                                bottom: 0,
-                                left: 20,
-                                right: 20,
-                                child: Container(
-                                  margin: EdgeInsets.symmetric(horizontal: 0),
-                                  height: 6,
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight,
-                                      stops: [
-                                        0,
-                                        1.0,
-                                      ],
-                                      colors: [
-                                        Color(0xFF594AF1),
-                                        Color(0xFFFF6B6B),
-                                      ],
-                                    ),
-                                  ),
-                                )),
-                            Container(
-                              width: MediaQuery.of(context).size.width - 40,
-                              margin: EdgeInsets.only(left: 20),
-                              child: SvgPicture.asset("assets/images/entry/desc.svg",fit: BoxFit.contain)
-                            ),
-                          ],
-                        ),)
+                        Expanded(
+                            child: Container(
+                                width: MediaQuery.of(context).size.width - 40,
+                                margin: EdgeInsets.only(left: 20),
+                                child: SvgPicture.asset(
+                                    "assets/images/entry/desc.svg",
+                                    fit: BoxFit.contain)))
                       ],
                     ),
-
                   ],
                 ),
               ),
@@ -86,25 +65,39 @@ class CreateAccountEntryPage extends StatelessWidget {
                 alignment: Alignment.center,
                 child: Padding(
                   padding: EdgeInsets.only(bottom: 60),
-                  child: Image.asset("assets/images/entry/auro_logo.png", width: MediaQuery.of(context).size.width * (245/ 375), height: MediaQuery.of(context).size.width * (245/ 375) * (221/245),),
+                  child: Image.asset(
+                    "assets/images/entry/auro_logo.png",
+                    width: MediaQuery.of(context).size.width * (245 / 375),
+                    height: MediaQuery.of(context).size.width *
+                        (245 / 375) *
+                        (221 / 245),
+                  ),
                 ),
               ),
-              Padding(padding: EdgeInsets.only(left: 38, right: 38),
+              Padding(
+                padding: EdgeInsets.only(left: 38, right: 38),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     minimumSize: Size(double.infinity, 48),
                     primary: ColorsUtil.hexColor(0x594AF1),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
                   ),
                   onPressed: () {
-                    _onClick('create');
+                    _onClick(context, 'create');
                   },
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       SvgPicture.asset("assets/images/entry/icon_add.svg"),
-                      SizedBox(width: 8,),
-                      Text(dic.createWallet, style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600))
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(dic.createWallet,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600))
                     ],
                   ),
                 ),
@@ -114,32 +107,44 @@ class CreateAccountEntryPage extends StatelessWidget {
                 child: OutlinedButton(
                   style: OutlinedButton.styleFrom(
                     minimumSize: Size(double.infinity, 48),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
                   ),
                   onPressed: () {
-                    _onClick('import');
+                    _onClick(context, 'import');
                   },
                   child: Row(
                     children: [
                       SvgPicture.asset("assets/images/entry/icon_restore.svg"),
-                      SizedBox(width: 8,),
-                      Text(dic.restoreWallet, style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 16, fontWeight: FontWeight.w600))
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(dic.restoreWallet,
+                          style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600))
                     ],
                   ),
                 ),
               ),
               Padding(
                 padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
-                child: Text(dic.restoreTip, style: theme.bodySmall?.copyWith(color: ColorsUtil.hexColor(0xCCCCCC)), textAlign: TextAlign.center,),
+                child: Text(
+                  dic.restoreTip,
+                  style: theme.bodySmall
+                      ?.copyWith(color: ColorsUtil.hexColor(0xCCCCCC)),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Padding(
                 padding: EdgeInsets.fromLTRB(0, 31, 0, 24),
-                child: Text('aurowallet.com', style: theme.bodySmall?.copyWith(color: ColorsUtil.hexColor(0xB9B9B9))),
+                child: Text('aurowallet.com',
+                    style: theme.bodySmall
+                        ?.copyWith(color: ColorsUtil.hexColor(0xB9B9B9))),
               ),
             ],
-          )
-      ),
+          )),
     );
   }
 }
-
