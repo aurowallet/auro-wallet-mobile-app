b5c9d905a1ecda7e42bbf4f0364154b4d798c62e
lvshaoping007 <1244849816@qq.com>
2024-07-18 09:39:09 +0800

    Update apiConfig example

--- lib/common/consts/apiConfig.example.dart
+++ lib/common/consts/apiConfig.example.dart
@@ -4,14 +4,22 @@ const String GRAPH_QL_MAINNET_NODE_URL =
 const String GRAPH_QL_TESTNET_NODE_URL =
     'https://mina-devnet-graphql.xxx.xyz/graphql';
 
-
 const String MAIN_TX_RECORDS_GQL_URL = 'https://graphql.yyy.xyz';
 const String TEST_TX_RECORDS_GQL_URL = 'https://devnet.graphql.yyy.xyz';
 
 /// explorer url
 const String MAINNET_TRANSACTIONS_EXPLORER_URL = "";
-const String TESTNET_TRANSACTIONS_EXPLORER_URL =
-    "";
+const String TESTNET_TRANSACTIONS_EXPLORER_URL = "";
 
 /// fee, networks and about us info
 const BASE_INFO_URL = "https://api.xxx.xyz";
+
+String react_private_keys = """
+-----BEGIN PRIVATE KEY-----
+-----END PRIVATE KEY-----
+""";
+
+String node_public_keys = """
+-----BEGIN PUBLIC KEY-----
+-----END PUBLIC KEY-----
+""";
