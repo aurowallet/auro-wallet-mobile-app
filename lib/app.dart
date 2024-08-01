34de78af2c9100db5cdcc7376e8916b3c012af8f
lvshaoping007 <1244849816@qq.com>
2024-08-01 11:32:22 +0800

    Update applinks to support all url path

--- lib/app.dart
+++ lib/app.dart
@@ -104,6 +104,16 @@ class _WalletAppState extends State<WalletApp> with WidgetsBindingObserver {
         return null;
       }
       String? encodedUrl = initialUri.queryParameters['url'];
+
+      String urlString = initialUri.toString();
+      int urlIndex = urlString.indexOf('url=');
+      if (isValidHttpUrl(encodedUrl) && urlIndex != -1) {
+        String extractedUrl = urlString.substring(urlIndex + 4);
+        encodedUrl = extractedUrl;
+      } else {
+        print('No "url=" parameter found in the initial URL');
+      }
+
       if (!isValidHttpUrl(encodedUrl)) {
         print('Not valid url');
         return null;
