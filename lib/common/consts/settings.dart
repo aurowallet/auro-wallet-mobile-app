9d442fcd686a2a8c2e3cff70711c290d5d1a3089
lvshaoping007 <1244849816@qq.com>
2024-08-05 22:35:57 +0800

    Upgrade version

--- lib/common/consts/settings.dart
+++ lib/common/consts/settings.dart
@@ -12,7 +12,7 @@ const int SECONDS_OF_DAY = 24 * 60 * 60; // seconds of one day
 const int SECONDS_OF_YEAR = 365 * 24 * 60 * 60; // seconds of one year
 
 /// app versions
-const String app_version = 'v2.1.0(1156)';
+const String app_version = 'v2.1.0(1157)';
 
 final Map<String, String> languageConfig = {
   "en": 'English',
