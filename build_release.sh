f91b76a6afc4323a7ed27f796aa5eb8f2ce36c98
lvshaoping007 <1244849816@qq.com>
2025-07-13 22:16:42 +0800

    Update build output

--- build_release.sh
+++ build_release.sh
@@ -17,7 +17,8 @@ version_for_zip=${version//+/-}
 main_version=$(echo $version | cut -d "+" -f1)
 
 # Define the package file names
-android_package="${name}_${version_with_dot}.apk"
+android_apk_package="${name}_${version_with_dot}.apk"
+android_aab_package="${name}_${version_with_dot}.aab"
 ios_package="${name}_${version_with_dot}.ipa"
 archive="${version_for_zip}.zip"
 
@@ -26,9 +27,13 @@ release_package_directory="release-package"
 target_directory="${release_package_directory}/${main_version}"
 mkdir -p $target_directory
 
-# Build the Android release package
+# Build the Android APK
 flutter build apk --release
-mv build/app/outputs/flutter-apk/app-release.apk $target_directory/$android_package
+mv build/app/outputs/flutter-apk/app-release.apk $target_directory/$android_apk_package
+
+# Build the Android App Bundle
+flutter build appbundle --release
+mv build/app/outputs/bundle/release/app-release.aab $target_directory/$android_aab_package
 
 # Build the iOS release package
 flutter build ipa --release
@@ -36,7 +41,7 @@ mv build/ios/ipa/Aurowallet.ipa $target_directory/$ios_package
 
 # Compress the packages into a single archive
 cd $target_directory
-zip $archive $android_package $ios_package
+zip $archive $android_apk_package $android_aab_package $ios_package
 cd ../..
 
 echo "Build and packaging completed. Archive created: $target_directory/$archive"
